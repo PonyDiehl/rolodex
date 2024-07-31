c097c8cea3753f9869353624dcf672a6f479e26e
Cowboy Crypto <dwight@cowboycrypto.io>
2024-07-31 15:16:51 -0700

    Component_State

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import './App.css';
 
 
 class App extends Component {
+constructor() {
+  super();
+
+  this.state = {
+    name: 'Bruh'
+  }
+}
+
+
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Hi There 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p> Hi {this.state.name} </p>
+          <button>Change Name</button>
         </header>
       </div>
     );
